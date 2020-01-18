a9989e65f30de384d79127ada078ce79fadab508
emilio <emilio@4b1a781e-1fcd-455b-a5aa-50a18b27795a>
2020-01-18 15:59:46 +0000

    relscf

--- tags/2020/scripts/mopacamk.py
+++ tags/2020/scripts/mopacamk.py
@@ -25,7 +25,7 @@ class MOPACamk(FileIOCalculator):
     default_parameters = dict(
         method='PM7',
         task='1SCF GRADIENTS bonds',
-        relscf=0.0001)
+        relscf=0.01)
 
     methods = ['AM1', 'MNDO', 'MNDOD', 'PM3', 'PM6', 'PM6-D3', 'PM6-DH+',
                'PM6-DH2', 'PM6-DH2X', 'PM6-D3H4', 'PM6-D3H4X', 'PMEP', 'PM7',
